9caff129f97986a411d3cb28744a431fc62ea331
KhalidHussanMajoka <108335078+KhalidHussanMajoka@users.noreply.github.com>
2022-11-11 07:42:41 -0800

    first commit

--- index.php
+++ index.php
@@ -982,28 +982,28 @@
                     <ul class="list-unstyled counter-one__list">
                         <li class="counter-one__single">
                             <div class="counter-one__count-box">
-                                <h3 class="odometer" data-count="70">00</h3>
+                                <h3 class="odometer" data-count="00">00</h3>
                                 <span class="counter-one__letter">m</span>
                             </div>
                             <p class="counter-one__text">Total donation</p>
                         </li>
                         <li class="counter-one__single">
                             <div class="counter-one__count-box">
-                                <h3 class="odometer" data-count="48">00</h3>
+                                <h3 class="odometer" data-count="00">00</h3>
                                 <span class="counter-one__letter">k</span>
                             </div>
                             <p class="counter-one__text">Projects funded</p>
                         </li>
                         <li class="counter-one__single">
                             <div class="counter-one__count-box">
-                                <h3 class="odometer" data-count="38">00</h3>
+                                <h3 class="odometer" data-count="00">00</h3>
                                 <span class="counter-one__letter">%</span>
                             </div>
                             <p class="counter-one__text">Kids need help</p>
                         </li>
                         <li class="counter-one__single">
                             <div class="counter-one__count-box">
-                                <h3 class="odometer" data-count="230">00</h3>
+                                <h3 class="odometer" data-count="00">00</h3>
                                 <span class="counter-one__letter"></span>
                             </div>
                             <p class="counter-one__text">Our volunteers</p>
